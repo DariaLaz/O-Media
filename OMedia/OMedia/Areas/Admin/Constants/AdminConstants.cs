dd2625d05b15da54731fab867c6ccb2a16526f2c
DariaLaz <daria.lazarova03@gmail.com>
2022-12-11 16:38:49 +0200

    Admin

---
+++ OMedia/OMedia/Areas/Admin/Constants/AdminConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace OMedia.Areas.Admin.Constants
+{
+    public static class AdminConstants
+    {
+        public const string AreaName = "Admin";
+        public const string AdminRoleName = "Administrator";
+        public const string AdminEmail = "admin@gmail.com";
+
+    }
+}
---
+++ OMedia/OMedia/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static OMedia.Areas.Admin.Constants.AdminConstants;
+namespace OMedia.Areas.Admin.Controllers
+{
+    [Area(AreaName)]
+    [Route("/Admin/[controller]/[Action]/{id?}")]
+    [Authorize(Roles = AdminRoleName)]
+    public class BaseController : Controller
+    {
+    }
+}
